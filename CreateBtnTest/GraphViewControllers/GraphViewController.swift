91526a4422e1c29c8d17521f2a64ba8a20850e9a
sherlockian4869 <ya0813kendo@gmail.com>
2021-01-17 12:04:15 +0900

    graphplus commit

--- CreateBtnTest/GraphViewControllers/GraphViewController.swift
+++ CreateBtnTest/GraphViewControllers/GraphViewController.swift
@@ -6,6 +6,8 @@ import Charts
 class GraphViewController: UIViewController {
     
     @IBOutlet weak var graphChartView: PieChartView!
+    @IBOutlet weak var graphTableView: UITableView!
+    
     var projectTitle: String?
     var projectId: String!
     var graphData = ["達成", "未達成"]
@@ -13,15 +15,22 @@ class GraphViewController: UIViewController {
     var Achievement: Double?
     var notAchieved: Double?
     var titleDatas = [String]()
+    var projectTime = [TimeModel]()
     
     private var firebase = Firestore.firestore()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpView()
+        fetchTimeDataFromFirestore()
     }
     
     func setUpView() {
+        graphTableView.delegate = self
+        graphTableView.dataSource = self
+        
+        navigationItem.title = "\(projectTitle!)　レポート"
+        
         guard let userId = Auth.auth().currentUser?.uid else { return }
         firebase.collection("user").document(userId).collection("project").document(projectId).getDocument { (document, err) in
             if err != nil {
@@ -74,4 +83,77 @@ class GraphViewController: UIViewController {
         
         view.addSubview(self.graphChartView)
     }
+    
+    private func fetchTimeDataFromFirestore() {
+        guard let userId = Auth.auth().currentUser?.uid else { return }
+        firebase.collection("user").document(userId).collection("project").document(projectId).collection("timer").addSnapshotListener { (snapshots, err) in
+            if err != nil {
+                print("Firebaseから情報受け取りに失敗しました")
+            }
+            
+            snapshots?.documentChanges.forEach({ (documentChange) in
+                switch documentChange.type {
+                case .added:
+                    let timeDocumentId = documentChange.document.documentID
+                    let timeObject = documentChange.document.data() as [String:AnyObject]
+                    let work = timeObject["work"]
+                    let rest = timeObject["rest"]
+                    let repeatCount = timeObject["repeat"]
+                    
+                    let timer = TimeModel(workTime: work as? String, restTime: rest as? String, repeatTime: repeatCount  as? String, timeId: timeDocumentId)
+                    self.projectTime.append(timer)
+                    
+                    self.graphTableView.reloadData()
+                    
+                case .modified, .removed:
+                    print("Nothing To Do")
+                }
+            })
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+}
+
+extension GraphViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return projectTime.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = graphTableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! GraphTableViewCell
+        let time: TimeModel
+        time = projectTime[indexPath.row]
+        cell.graphLabel.text = "\(time.workTime!)分×\(time.restTime!)分　\( time.repeatTime!)回"
+        return cell
+    }
+    
+    
+}
+
+class GraphTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var graphLabel: UILabel!
+    @IBOutlet weak var graphView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        graphView.layer.cornerRadius = 10
+        // セル選択時の色を変更
+        selectedBackgroundView = makeSelectedBackgroundView()
+    }
+    // セル選択時の背景色をを白にする処理
+    private func makeSelectedBackgroundView() -> UIView {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
 }
